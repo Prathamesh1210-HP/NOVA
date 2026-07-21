b6de7209d2fd3173b3bad01928921b1c93b6bb00
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-07-21 17:51:59 +0530

    checking

--- NOVA.py
+++ NOVA.py
@@ -24,7 +24,7 @@ import smtplib
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[0].id)
-engine.setProperty('voices', voices[0].id)
+engine.setProperty('voices',voices[0].id)
 
 
 #text to speech
