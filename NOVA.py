c42adbf108acb970f3834131b22231e93d3498f9
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-08 11:29:01 +0530

    check

--- NOVA.py
+++ NOVA.py
@@ -43,7 +43,7 @@ def takecommand():
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")
+        print("Recognizing....")
         query = r.recognize_google(audio,language='en-in')
         print(f"User said:{query}")
 
